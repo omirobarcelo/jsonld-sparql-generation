7d51ad81f6b7fe6a25a10260c3419ae1c688e7e1
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2020-04-08 14:54:50 +0200

    Basic form

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,34 @@
 <script>
-	let name = 'world';
+  import Button, { Label as BtnLabel, Icon } from "@smui/button";
+  import Textfield from "@smui/textfield";
+  import HelperText from "@smui/textfield/helper-text/index";
+
+  let weight = 0;
+  let show = false;
+
+  function showData() {
+    show = true;
+  }
 </script>
 
-<h1>Hello {name}!</h1>
+<div class="container">
+  <form on:submit|preventDefault={showData}>
+    <Textfield
+      type="number"
+      bind:value={weight}
+      label="Weight (kg)"
+      input$min="0"
+      input$step="0.1"
+      input$aria-controls="helper-text-weight"
+      input$aria-describedby="helper-text-weight" />
+    <HelperText id="helper-text-weight">Ex.: 40.2</HelperText>
+
+    <Button>
+      <BtnLabel>Generate</BtnLabel>
+    </Button>
+  </form>
+
+  {#if show}
+    <p class="mdc-typography--body1">{weight}kg</p>
+  {/if}
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-const sveltePreprocess = require('svelte-preprocess');
+const sveltePreprocess = require("svelte-preprocess");
 
 module.exports = {
-    // we'll extract any component CSS out into
-    // a separate file  better for performance
-    css: css => {
-        css.write('public/build/bundle.css');
-    },
-    preprocess: sveltePreprocess()
+  // emits CSS as "files" for other plugins to process
+  emitCss: true,
+  // we'll extract any component CSS out into
+  // a separate file  better for performance
+  css: (css) => {
+    css.write("public/build/bundle.css");
+  },
+  preprocess: sveltePreprocess(),
 };
